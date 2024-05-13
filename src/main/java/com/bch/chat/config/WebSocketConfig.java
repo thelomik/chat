1442651e3443c1abced205f1ddde26a8141b0cc3
thelomik <akardos11@gmail.com>
2024-05-13 21:35:22 +0200

    frond end

--- src/main/java/com/bch/chat/config/WebSocketConfig.java
+++ src/main/java/com/bch/chat/config/WebSocketConfig.java
@@ -25,9 +25,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
-                .withSockJS();
+   public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins().withSockJS();
     }
 
     @Override
